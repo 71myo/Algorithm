27eea4516f017dad7e733a932196dc5fba3fdd4c
Hyojeong <abadeer@naver.com>
2025-03-10 03:37:19 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.38 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.swift
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.swift
@@ -1,12 +1,7 @@
 import Foundation
 
 func solution(_ my_string:String, _ s:Int, _ e:Int) -> String {
-    let startIndex = my_string.index(my_string.startIndex, offsetBy: s)
-    let endIndex = my_string.index(my_string.startIndex, offsetBy: e)
-
-    let first = my_string[..<startIndex]
-    let middle = my_string[startIndex...endIndex].reversed()
-    let end = my_string[endIndex...].dropFirst()
-    
-    return String(first + middle + end)
+    var my_string = Array(my_string)
+    my_string.replaceSubrange(s...e, with: Array(my_string[s...e].reversed()))
+    return String(my_string)
 }
