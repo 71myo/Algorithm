8aaac44833ff6032e17c90067fb6147241875b21
Hyojeong <abadeer@naver.com>
2025-07-27 21:53:45 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ date1:[Int], _ date2:[Int]) -> Int {
+    if (date1[0], date1[1], date1[2]) < (date2[0], date2[1], date2[2]) {
+        return 1
+    } else {
+        return 0
+    }
+}
